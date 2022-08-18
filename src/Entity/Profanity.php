c2de9568a874468d238eb5c90d59be4e652ba54b
Harry de Boer <hldeboer@gmail.com>
2022-08-18 17:06:10 +0200

    profanity name is unique

--- src/Entity/Profanity.php
+++ src/Entity/Profanity.php
@@ -6,11 +6,14 @@ namespace App\Entity;
 
 use App\Repository\ProfanityRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[
     ORM\Entity(repositoryClass: ProfanityRepository::class),
     ORM\Table(name: "profanity"),
+    ORM\UniqueConstraint(fields: ["name"]),
+    UniqueEntity(fields: ["name"], message: "Er is al een scheldwoord met deze naam."),
 ]
 class Profanity
 {
