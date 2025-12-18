b9763a7d870e0a860110539b76e995241664505a
Harry de Boer <hldeboer@gmail.com>
2025-12-18 17:40:00 +0100

    jquery and bootstrap update

--- src/EventListener/Listener.php
+++ src/EventListener/Listener.php
@@ -18,13 +18,13 @@ class Listener
         $response->headers->set(
             'Content-Security-Policy', "default-src 'self'; style-src 'self' " .
             "https://fonts.googleapis.com/css2 " .
-            "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css " .
+            "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css " .
             "https://www.gstatic.com; " .
             "font-src 'self' https://fonts.gstatic.com; " .
             "img-src 'self' data:; " .
             "script-src 'self' https://www.googletagmanager.com/gtag/js " .
-            "https://code.jquery.com/jquery-3.6.3.min.js " .
-            "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js " .
+            "https://code.jquery.com/jquery-3.7.1.min.js " .
+            "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js " .
             "https://www.google.com/recaptcha/api.js https://www.gstatic.com; " .
             "frame-src 'self' https://www.google.com/",
         );
