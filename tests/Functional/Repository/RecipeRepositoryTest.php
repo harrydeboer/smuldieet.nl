72a9a904bb013c6a388b3ff079675b2d46bcc360
Harry de Boer <hldeboer@gmail.com>
2025-12-24 15:02:14 +0100

    recipe test update

--- tests/Functional/Repository/RecipeRepositoryTest.php
+++ tests/Functional/Repository/RecipeRepositoryTest.php
@@ -41,7 +41,7 @@ class RecipeRepositoryTest extends KernelTestCase
         $this->assertSame($updatedTitle, $recipeRepository->findOneBy(['title' => $updatedTitle])->getTitle());
         $this->assertSame($recipe, $recipeRepository->getFromUser($recipe->getId(), $userId));
         $this->assertSame([$recipe], $recipeRepository->search($recipe->getTitle(), $recipe->getUser()->getId()));
-        $this->assertTrue(in_array($recipe, $recipeRepository->findRecent(1)->getResults()));
+        $this->assertTrue(in_array($recipe, $recipeRepository->findRecent(2)->getResults()));
         $this->assertTrue(in_array($recipe, $recipeRepository->findBySortAndFilter(1)->getResults()));
         $this->assertTrue(in_array($recipe, $recipeRepository->getRecipesFromUser($userId, 1)->getResults()));
         $this->assertSame([$recipePending], $recipeRepository->findAllPending());
